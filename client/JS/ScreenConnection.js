f53dbc6b3acb7ae86e585f02224580cf23a604c8
boblecodeur <tristanplouz@gmail.com>
2014-08-30 15:43:54 +0200

    Modification de ScreenConnection

--- client/JS/ScreenConnection.js
+++ client/JS/ScreenConnection.js
@@ -4,5 +4,22 @@ function ScreenConnection(){
 		this.modifIP=function(){
 			qs('#modifIP').style.display = "block";
 		}
+		this.connectSuccess=function(){
+			
+			var user = qs("#username").value;
+			var team = $(‘input[type=radio][name=team]:checked’).val();
+			
+			var client = new ClientJoueur();
+			client.user = user;
+			client.team = team;
+			
+			var data ={
+						"object": "login",
+						"username": user,
+						"team": team
+						};
+
+			Client.send(data);
+		}
 }
-ScreenConnection.prototype = new Screen;
+ScreenConnection.prototype = new Screen();
