1c9ccf8ca46328ab154c85b2a921cf245a90b684
naegi <tritri83@hotmail.fr>
2014-08-30 16:49:03 +0200

    ajout du chargement auto des fichiers

--- client/JS/Screen.js
+++ client/JS/Screen.js
@@ -1,5 +1,9 @@
 function Screen( sec ){
 		this.section = sec;
+        this.load = function(file,id){
+            $('body').append('<section id =' + id + '>'); 
+            $('#' + id).load(file);
+        }
 		this.open = function(){
 			this.section.style.display = "block";
 		};
