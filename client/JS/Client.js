e50090fbceba13d7b5d6ca647e66c5abf5d79fd3
naegi <tritri83@hotmail.fr>
2014-08-30 15:49:59 +0200

    envoie d'un signal a connection co lors de la co

---
+++ client/JS/Client.js
@@ -0,0 +1,47 @@
+function Client()
+{
+    var webSocket = false;
+    var ws = new wsLib();
+    
+    this._onmessage = function(e){ 
+        rep = JSON.parse(e.data);
+        console.log(rep);
+        this.onMessage(data)
+    }
+
+    this.onMessage = function(data){
+    } 
+
+    this.onError = function(e){
+    }
+    
+    this.onClose = function(){
+        var data = {object :"logout"};
+        this.send(data);
+        webSocket = false;
+    }functionfunction
+    
+    this.openConnection = function (ip){
+        if(!webSocket){
+            ws.openSocket(ip,
+                this.onConnection,
+                this.onClose,
+                this.onMessage,
+                this.onError );
+        }
+    }
+    
+    this.onConnection = function(){
+        webSocket = true;
+        ScreenConnection.connectSucess();
+    }
+
+    this.send = function (data){
+        if(webSocket && ! ws.isClosed()){
+            ws.msg(data);
+        }
+        else{
+            ws.close();
+        }
+    }
+}
