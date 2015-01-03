927dfa3929c4915860657accbc182ee7b1bc90aa
boblecodeur <tristanplouz@gmail.com>
2015-01-03 16:04:04 +0100

    modification des marqueurs

--- client/JS/ScreenMap.js
+++ client/JS/ScreenMap.js
@@ -84,10 +84,10 @@ function ScreenMap(){
 			client.send(data);
 			
 			if(!this.myMarker){
-				this.myMarker = L.marker([lat,lon],{icon: this.lambdaPlayer}).addTo(this.map);
+				this.myMarker = L.marker([lat,lon],{icon: this.kill}).addTo(this.map);
 			}
 			else{
-				this.myMarker.setIcon(this.lambdaPlayer).setLatLng([lat,lon]);
+				this.myMarker.setIcon(this.kill).setLatLng([lat,lon]);
 			}
 			
 		};
