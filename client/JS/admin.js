0fdc6f76d3c37756eca575696dec91306bcd57f5
boblecodeur <tristanplouz@gmail.com>
2015-02-19 18:32:25 +0100

    modification minue

--- client/JS/admin.js
+++ client/JS/admin.js
@@ -1,26 +1,28 @@
 function Admin (){
 	this.zonesTable=[];
 	var that=this;
+	
 	this.defZone=function(){
-					var index=0;
+		var index=0;
 
-					map.on('click', function(e) {
-						that.zonesTable.push(zone.zoneCreate(index,[e.latlng.lat,e.latlng.lng],qs("#radius").value,qs('#team').value));
-						console.log(that.zonesTable);
-					switch(that.zonesTable[index].team){
-					case 'tidu':
-						L.circle(that.zonesTable[index].pos,that.zonesTable[index].radius,{color:'red'}).addTo(map);
-						break;
-					case 'tizef':
-						L.circle(that.zonesTable[index].pos,that.zonesTable[index].radius).setStyle({color:'blue'}).addTo(map);
-						break;
-					case 'neutre':
-						L.circle(that.zonesTable[index].pos,that.zonesTable[index].radius).setStyle({color:'grey'}).addTo(map);
-						break;
-				}
-						index++;
-					});
-				};
+		map.on('click', function(e) {
+			that.zonesTable.push(zone.zoneCreate(index,[e.latlng.lat,e.latlng.lng],qs("#radius").value,qs('#team').value));
+			console.log(that.zonesTable);
+
+		switch(that.zonesTable[index].team){
+		case 'tidu':
+			L.circle(that.zonesTable[index].pos,that.zonesTable[index].radius,{color:'red'}).addTo(map);
+			break;
+		case 'tizef':
+			L.circle(that.zonesTable[index].pos,that.zonesTable[index].radius).setStyle({color:'blue'}).addTo(map);
+			break;
+		case 'neutre':
+			L.circle(that.zonesTable[index].pos,that.zonesTable[index].radius).setStyle({color:'grey'}).addTo(map);
+			break;
+		}
+		index++;
+		});
+	};
 	this.time=function(){
 		var time =qs("#timer").value;
 		time = time*60;
